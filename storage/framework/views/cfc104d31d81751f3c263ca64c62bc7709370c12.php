8d7176cacd53d20399c1e2235e4e3c7c8257180a
game-devel1 <rahatsadykov784@gmail.com>
2020-09-08 18:49:15 +0600

    change first section in main page

--- storage/framework/views/cfc104d31d81751f3c263ca64c62bc7709370c12.php
+++ storage/framework/views/cfc104d31d81751f3c263ca64c62bc7709370c12.php
@@ -2,40 +2,40 @@
     <div class="centerslider-1">
         <div class="holder">
             <div class="img">
-                <img src="custom2/img/JanQuaty_banner.jpeg" alt="image description">
+                <img src="custom2/img/janquaty_banner.jpg" alt="image description">
             </div>      
             <div class="caption">
                 <div class="c1">
                     <div class="c2">
                         <h1>Jan Quaty</h1>
-                        <h2> <?php echo app('translator')->get('app.jan_quaty_text'); ?> </h2>
-                    </div>
+                        <h2> <?php echo app('translator')->get('app.jan_quaty_text'); ?> </h2>                    
+                    </div>                    
                 </div>
             </div>          
         </div>
         <div class="holder">
             <div class="img">
-                <img src="custom2/img/JanTazalygy_banner.jpeg" alt="image description">
+                <img src="custom2/img/jantazalygy_banner.jpg" alt="image description">
             </div>
             <div class="caption">
                 <div class="c1">
                     <div class="c2">
                         <h1>Jan Tazalygy</h1>
-                        <h2> <?php echo app('translator')->get('app.jan_tazalygy_text'); ?> </h2>
-                    </div>
+                        <h2 class="text-reverse"> <?php echo app('translator')->get('app.jan_tazalygy_text'); ?> </h2>                        
+                    </div>                    
                 </div>
             </div>
         </div>
         <div class="holder">
             <div class="img">
-                <img src="custom2/img/JanTynyshtygy_banner.jpeg" alt="image description">
+                <img src="custom2/img/jantynyshtygy_banner.jpg" alt="image description">
             </div>
             <div class="caption">
                 <div class="c1">
                     <div class="c2">
                         <h1>Jan Tynyshtygy</h1>
-                        <h2> <?php echo app('translator')->get('app.jan_tynyshtygy_text'); ?> </h2>
-                    </div>
+                        <h2> <?php echo app('translator')->get('app.jan_tynyshtygy_text'); ?> </h2>                        
+                    </div>                    
                 </div>
             </div>
         </div>
