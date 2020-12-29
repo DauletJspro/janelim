95d313444b6608e394d5f14e968f59d19383d3bb
daulet <sailauovdaulet15@gmail.com>
2020-12-29 15:13:41 +0500

    fix bugs for gv and user money 3

--- database/seeds/FixAndChangeMoneyPvGv.php
+++ database/seeds/FixAndChangeMoneyPvGv.php
@@ -12,36 +12,13 @@ class FixAndChangeMoneyPvGv extends Seeder
     public function run()
     {
         $userSeven = \App\Models\Users::where('user_id', '=', '4')->first();
-        $userSeven->user_money = $userSeven->user_money + 28;
+        $userSeven->user_money = $userSeven->user_money + 15.8;
         $userSeven->save();
 
         $userSeven = \App\Models\Users::where('user_id', '=', '7')->first();
-        $userSeven->user_money = $userSeven->user_money + 40.63;
+        $userSeven->user_money = $userSeven->user_money + 3.6;
         $userSeven->save();
 
-        $userSeven = \App\Models\Users::where('user_id', '=', '9')->first();
-        $userSeven->user_money = $userSeven->user_money + 19;
-        $userSeven->save();
-
-        $userSeven = \App\Models\Users::where('user_id', '=', '317')->first();
-        $userSeven->user_money = $userSeven->user_money + 3;
-        $userSeven->save();
-
-        $userSeven = \App\Models\Users::where('user_id', '=', '5')->first();
-        $userSeven->user_money = $userSeven->user_money + 1.2;
-        $userSeven->save();
-
-        $userSeven = \App\Models\Users::where('user_id', '=', '8')->first();
-        $userSeven->user_money = $userSeven->user_money + 2.4;
-        $userSeven->save();
-
-        $userSeven = \App\Models\Users::where('user_id', '=', '201')->first();
-        $userSeven->user_money = $userSeven->user_money + 2.4;
-        $userSeven->save();
-
-        $userSeven = \App\Models\Users::where('user_id', '=', '235')->first();
-        $userSeven->user_money = $userSeven->user_money + 1.2;
-        $userSeven->save();
 
     }
 }
