a91d2602a5d80c4d21488f0ef8438e4fff098eb5
daulet <sailauovdaulet15@gmail.com>
2021-01-20 16:01:40 +0500

    Fix bugs with bonus distrubition

--- database/seeds/ChangeImgPacket.php
+++ database/seeds/ChangeImgPacket.php
@@ -12,20 +12,8 @@ class ChangeImgPacket extends Seeder
      */
     public function run()
     {
-        DB::table('packet')->where('packet_id', '=', 1)->update([
-           'packet_image' => '/media/Small-min_1 (1).jpeg'
-        ]);
-        DB::table('packet')->where('packet_id', '=', 2)->update([
-            'packet_image' => '/media/Medium-min-min (1)-min.png'
-        ]);
         DB::table('packet')->where('packet_id', '=', 3)->update([
-            'packet_image' => '/media/Large-min (1).jpeg'
-        ]);
-        DB::table('packet')->where('packet_id', '=', 4)->update([
-            'packet_image' => '/media/VIP-min (1).jpeg'
-        ]);
-        DB::table('packet')->where('packet_id', '=', 5)->update([
-            'packet_image' => '/media/LUX-min (1).jpeg'
+            'packet_image' => '/media/Medium-last.jpeg'
         ]);
 
     }
--- resources/views/new_design/index/_first_section.blade.php
+++ resources/views/new_design/index/_first_section.blade.php
@@ -2,17 +2,17 @@
     <div class="centerslider-1">
         <div class="holder">
             <div class="img">
-                <img src="custom2/img/Jan Elim.png" alt="image description">
+                <img src="media/6.png" alt="image description">
             </div>                
         </div>
         <div class="holder">
             <div class="img">
-                <img src="custom2/img/Jan Elim4.png" alt="image description">
+                <img src="media/img.png" alt="image description">
             </div>                
         </div>
         <div class="holder">
             <div class="img">
-                <img src="custom2/img/Jan Elim5.png" alt="image description">
+                <img src="media/5.png" alt="image description">
             </div>                
         </div>
     </div>
