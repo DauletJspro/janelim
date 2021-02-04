5890802b2db96a9be904cf1828a2129b2acf8179
daulet <sailauovdaulet15@gmail.com>
2021-02-04 10:27:32 +0500

    Change packet image

--- database/seeds/ChangeImgPacket.php
+++ database/seeds/ChangeImgPacket.php
@@ -12,8 +12,24 @@ class ChangeImgPacket extends Seeder
      */
     public function run()
     {
+        DB::table('packet')->where('packet_id', '=', 1)->update([
+            'packet_image' => '/media/small.png'
+        ]);
+
         DB::table('packet')->where('packet_id', '=', 2)->update([
-            'packet_image' => '/media/Medium-last.jpeg'
+            'packet_image' => '/media/Medium.png'
+        ]);
+
+        DB::table('packet')->where('packet_id', '=', 3)->update([
+            'packet_image' => '/media/Large-min (1) (1).png'
+        ]);
+
+        DB::table('packet')->where('packet_id', '=', 4)->update([
+            'packet_image' => '/media/VIP.png'
+        ]);
+
+        DB::table('packet')->where('packet_id', '=', 5)->update([
+            'packet_image' => '/media/LUX.png'
         ]);
 
     }
